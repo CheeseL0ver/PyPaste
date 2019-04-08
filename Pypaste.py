6cff82f40548dd7534e8dda30e6b8a56912f39c7
Shandon Anderson <andersons3@appstate.edu>
2019-04-08 16:36:01 -0400

    Added integration for pastebin

--- Pypaste.py
+++ Pypaste.py
@@ -1,12 +1,26 @@
-import sys
-import click
+#!/usr/local/bin/python3.7
 
-text = ''
+import sys, requests
 
-if not sys.stdin.isatty():
-    for line in sys.stdin:
-        text += line
+class Main():
+    text = ''
+    PASTEBIN_DEV_KEY = ''
 
-if text != '':
-    print (text)
+    def readStdin(self):
+        if not sys.stdin.isatty():
+            for line in sys.stdin:
+                self.text += line
 
+    def post(self):
+        payload = {'api_option': 'paste', 'api_dev_key': self.PASTEBIN_DEV_KEY, 'api_paste_code': self.text}
+        r = requests.post('https://pastebin.com/api/api_post.php', data=payload)
+        print(r.text)
+
+    def main(self):
+        self.readStdin()
+        if self.text != '':
+            #print (self.text)
+            self.post()
+
+if __name__ == '__main__':
+    Main().main()
