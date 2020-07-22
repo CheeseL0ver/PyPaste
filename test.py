8bf6611ff663ab5fa02f2e27e6636619c46adab3
Shandon Anderson <shandon@flygone.io>
2020-07-22 12:04:56 -0400

    Updated unit tests

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import unittest
-from pypaste.__main__ import post
+from pypaste.__main__ import main
 class TestSum(unittest.TestCase):
 
   def testPaste(self):
-    self.assertEqual(post(), 0, 'Should be zero.')
+    self.assertEqual(main(), 0, 'Should be zero.')
 
 if __name__ == '__main__':
   unittest.main()
